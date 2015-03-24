0aec72653cf0407352e350e0e67babc73a124fe5
lemmi <markus@lemmert.net>
2015-03-24 21:38:06 +0100

    crunchbangmint.sh obmenu obconf corrected

--- crunchbangmint.sh
+++ crunchbangmint.sh
@@ -3,4 +3,4 @@
 # this script will install the packages needed vor a crunchbanglike openbox DE. It is recommended to run sudo apt-get update first
 # by markuslemmert@gmail.com
 
-sudo apt-get install -y openbox dbconf dbmenu tint2 xcompmgr nitrogen hsetroot
+sudo apt-get install -y openbox obconf obmenu tint2 xcompmgr nitrogen hsetroot
