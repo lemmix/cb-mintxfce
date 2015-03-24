6bf35165be79334c0ce82d55fb7f5a4a59b1726b
lemmix <markuslemmert@gmail.com>
2015-03-24 12:03:54 +0100

    Create crunchbangmint.sh

---
+++ crunchbangmint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this script will install the packages needed vor a crunchbanglike openbox DE. It is recommended to run sudo apt-get update first
+# by markuslemmert@gmail.com
+
+sudo apt-get install -y openbox dbconf dbmenu tint2 xcompmgr nitrogen hsetroot
