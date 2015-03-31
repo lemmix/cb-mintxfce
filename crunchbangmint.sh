397ea545fffde8439064714f8e1de63a7ad9f2e8
lemmi <markus@lemmert.net>
2015-03-31 21:58:36 +0200

    crunchbangmint updated, without dialog using read

--- crunchbangmint.sh
+++ crunchbangmint.sh
@@ -3,59 +3,67 @@
 # this script will install the packages needed vor a crunchbanglike openbox DE. It is recommended to run sudo apt-get update first
 # by markuslemmert@gmail.com
 
-DIALOG=${DIALOG=dialog}
+read -p "Installer: Do you really want to crunchbang your xfce?(y/n):" answer
 
-$DIALOG --title " crunchbang the Mint Xfce" --clear \
-        --yesno "Do you really want to crunchbang your Xfce?" 10 30
-
-case $? in
-  0)
-    clear && sudo apt-get install -y openbox obconf obmenu tint2 xcompmgr nitrogen hsetroot volti;;
-  1)
-    clear && exit 1;;
-  255)
-    clear && exit 1;;
+case "$answer" in
+        Yes|yes|Y|y|"") echo "Ok..."
+            sudo apt-get install -y openbox obconf obmenu tint2 xcompmgr nitrogen hsetroot;;
+        No|no|N|n) echo "Aborted."
+            exit 1
+            ;;
+            *) echo "Unknown parameter" 
+            ;;
 esac
 
 if [ $? -ne 0 ]
 then
-echo "Install failed!" >&2
+	echo "Something went wrong! One or more packages...[Failed]" >&2
 exit 1
 else 
-echo "Packages installed!" && sleep 3 
+	echo "Packages installed...[OK]" && sleep 3 
 fi
 
 sudo cp cb* /usr/local/bin/ && sudo chmod +x /usr/local/bin/cb*
 
 if [ $? -ne 0 ]
 then
-echo "CP failed!" >&2
-sleep 3 && exit 1
+	echo "CP failed!" >&2
+	sleep 3 && exit 1
 else 
-echo "Pipemenus copied!" && sleep 3 
+	echo "Pipemenus copied...[OK]" && sleep 3 
 fi
 
 
-$DIALOG --title " crunchbang the Mint Xfce" --clear \
-        --yesno "Make Folder ~/.config/openbox/ and cp Files into it? " 10 30
+read -p "Make folder ~/.config/openbox/ and copy files into it?(y/n):" answer
 
-case $? in
-  0)
-    clear && sudo mkdir ~/.config/openbox/ && sudo cp menu.xml rc.xml autostart ~/.config/openbox/;;
-  1)
-    clear && echo "Standard files will be used!";;
-  255)
-    clear && echo "Standard files will be used!";;
+case "$answer" in
+        Yes|yes|Y|y|"") echo "Ok..."
+            sudo mkdir ~/.config/openbox/ && sudo cp menu.xml rc.xml autostart ~/.config/openbox/;;
+        No|no|N|n) echo "Standard files will be used!"
+            ;;
+            *) echo "Unknown parameter! Standard files will be used ..." 
+            ;;
 esac
+
 
 if [ $? -ne 0 ]
 then
-echo "CP failed!" >&2
+	echo "CP failed!" >&2
 exit 1
 fi
 
 sleep 3
 
-$DIALOG --title "Finished" --msgbox 'Now you crunchbanged your Xfce - Log out and choose openbox as DE' 10 30
-clear
-cb-exit && exit 0
+read -p "Finished...! Do you want to log out now?(y/n):" answer
+
+case "$answer" in
+        Yes|yes|Y|y|"") echo "Ok..."
+            openbox --exit;;
+        No|no|N|n) echo "Well you can use xfce, no problem..."
+            exit 0
+            ;;
+            *) echo "Unknown parameter" 
+            ;;
+esac
+
+exit 0
